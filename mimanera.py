cd06bb241d7ed3baecd1db114fdf0010e39fd89d
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-05-12 00:47:25 +0200

    iuiuib

--- mimanera.py
+++ mimanera.py
@@ -138,211 +138,38 @@ def ordenar_por_ts(navegacion_filtrada):
     return navegacion_filtrada
 print(ordenar_por_ts(dataset_navegacion()))
 
+# PASO 4: UNIR CSVS
+# limpia el csv conversiones
+def Limpiar_conversiones(dato=[]):
+    for i in range (len(dato)):
+        if (dato[i] == 'None'):
+            dato[i] = ""
+    return dato
+    # aplica la funcion limpiar a las columnas 'id_user y ' gclid'de conversiones
+Limpiar_conversiones(dataset_conversiones['id_user'])
+Limpiar_conversiones(dataset_conversiones['gclid'])
+conversion_final = pd.DataFrame({'id_user':Limpiar_conversiones(dataset_conversiones()['id_user']), 'gclid':Limpiar_conversiones(dataset_conversiones()['gclid']),'date': dataset_conversiones (), 'hour':dataset_conversiones()['hour'], 'id_lead':dataset_conversiones()['id_lead'], 'lead_type': dataset_conversiones()['lead_type'],'result':dataset_conversiones()['result']})
+conversion_final.to_csv("conversiones_final.csv", sep = ";")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-def juntarTablas(Dataframe1,Dataframe2):
-
-    Dataframe1= eliminarRepetidos(Dataframe1)
-    Dataframe2= eliminarRepetidos(Dataframe2)
-    Dataframe3= pd.DataFrame()
-    if "id_suite" in Dataframe1.columns and "id_suite" in Dataframe2.columns:
-        Dataframe3= pd.merge(Dataframe1, Dataframe2, on='id_suite', how='outer',suffixes=("_nav","_conv"))
-    elif "gclid" in Dataframe1.columns and "gclid" in Dataframe2.columns:
-        Dataframe3= pd.merge(Dataframe1, Dataframe2, on='gclid', how='outer',suffixes=("_nav","_conv"))
-    else:
-        Dataframe3= pd.merge(Dataframe1, Dataframe2, on='url_landing', how='outer',suffixes=("_nav","_conv"))
-
-    return Dataframe3
-
-conv=pd.DataFrame(conversiones)
-nav= nav.assign(Convertido=0)
-conv=conv.assign(Convertido=1)
-union= juntarTablas(nav, conv)
-union.to_csv("union.csv", index=False,sep=";")
-conversiones = pd.read_csv("conversiones.csv", sep = ";")
-navegacion = pd.read_csv("navegacion_filtrada.csv", sep = ";")
-def filtrar_conversiones():
-    index = []
-    for i in range(conversiones.shape[0]):
-        if conversiones._get_value(i, "id_user") == "None":
-            index.append(i)
-
-    conversiones = pd.DataFrame(conversiones.drop(conversiones.index[index]), columns = conversiones.columns)
-    conversiones = conversiones.reset_index()
-
-#Numero de visitas que recibe el cliente
-    print("El numero de visitas que recibe es:", navegacion.shape[0], "visitas")
-
-def conversiones_por_cliente():
-#Cuántas de ellas convierten y cuántas no (en %)
-    convertidos = 0
-    users_navegacion = navegacion["id_user"]
-    users_conversion = conversiones["id_user"]
-    for user_nav in users_navegacion:
-        for user_conv in users_conversion:
-            if user_nav == user_conv:
-             convertidos += 1
-
-    print("El porcentaje de visitas a convertidos es de " , convertidos / navegacion.shape[0] * 100, end="%\n")
-
-#Por tipo de conversión (CALL o FORM), ¿cuántas hay de cada una?
-#Primero observamos que no hay ningun user repetido, a simple vista se ve
-def  tipo_conversion():
-    call = 0
-    form = 0
-    tipo = conversiones["lead_type"]
-    for type in tipo:
-        if type == "CALL":
-            call += 1
+def Conversiones(data1, data2):
+    conversion = []
+    for i in data1:
+        if i in data2:
+            conversion.append(1)
         else:
-            form += 1
-
-    print("EL numero de conversiones del tipo call es:", call)
-    print("EL numero de conversiones del tipo form es:", form)
-
-#Porcentaje de usuarios recurrentes sobre el total de usuarios
-#Solo nos interesa una vez cada uno de los users asi que los guardamos en un conjunto para que no haya repetidos
-def usuarios_recurrentes():
-    recurrente = {()}
-    number_of_users = {()}
-
-    for i in range(navegacion.shape[0]):
-        number_of_users.add(navegacion._get_value(i, "id_user"))
-        if navegacion._get_value(i, "user_recurrent") == True:
-         recurrente.add(navegacion._get_value(i, "id_user"))
-    print("El porcentaje de usuarios recurrentes frente a los totales es de:", len(recurrente) / len(number_of_users) * 100, end="%\n")
-
-#Unir a la tabla de navegacion si convierte o no.
-def unir_conversiones():
-    data = []
-    only_user = navegacion["id_user"]
-    for user in only_user:
-        convierte = False
-        for i in conversiones["id_user"]:
-            if user == i:
-                convierte = True
-    
-        if convierte:
-            data.append(1)
-        else:
-            data.append(0)
-
-    navegacion["convierte"] = data
-#Ahora navegacio contiene todos los datos de si ese usuario convierte o no.
-
-#Ver cual es el coche más visitado de la página.
-def coche_mas_visitado():
-    cars = {
-    
-    }
-
-    for i in range(navegacion.shape[0]):
-        m = re.search("http(?:s?):\/(?:\/?)www\.metropolis\.com\/es\/(.+?)\/.*", str(navegacion._get_value(i, "url_landing")))
-        if m != None:
-            if m.groups()[0] in cars:
-                cars[m.groups()[0]] += 1
-            else:
-                cars[m.groups()[0]] = 1
-        
-    for car in cars.keys():
-        print("El coche", car, "ha sido buscado", cars[car], "veces")
-
-def separar_datos():
-#Separar cada una de las partes para añair a la tabla
-    campaña = []
-    adg = []
-    adv = []
-    sl = []
-    urls = navegacion["url_landing"]
-#Valor del id campaña
-    for url in urls:
-        try:
-            esp = str(url).split("camp=")
-            bueno = esp[1].split("&")
-            campaña.append(bueno[0])
-        except:
-            campaña.append(0)
-#Valor del id del adgroup
-    for url in urls:
-        try:
-            esp = str(url).split("adg=")
-            bueno = esp[1].split("&")
-            adg.append(bueno[0])
-        except:
-            adg.append(0)
-#valor del adv
-    for url in urls:
-        try:
-            esp = str(url).split("adv=")
-            bueno = esp[1].split("&")
-            adv.append(bueno[0])
-        except:
-            adv.append(0)
-#valor del sl
-    for url in urls:
-        try:
-            esp = str(url).split("sl=")
-            bueno = esp[1].split("&")
-            sl.append(bueno[0])
-        except:
-            sl.append(0)
-    navegacion["id_camp"] = campaña
-    navegacion["id_adg"] = adg
-    navegacion["id_adv"] = adv
-    navegacion["id_sl"] = sl
-
-    print(navegacion)
-#Esto es para guardar el fichero final, comentar si no se usa
-    navegacion.to_csv("navegacion_final.csv", index = False)
-
-# ejecuta las funciones
-
-unir_conversiones()
-separar_datos()
-coche_mas_visitado()
-conversiones_por_cliente()
-tipo_conversion()
-usuarios_recurrentes()
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+            conversion.append(0)
+    return conversion
+# aplicamos la funcion a las columnas id_user y gclid 
+navegacion_final = pd.read_csv("navegacion_filtrada.csv", sep = ";")
+conversion_final = pd.read_csv("conversiones_final.csv", sep = ";")
+conversion_por_id= Conversiones(navegacion_final['id_user1'], conversion_final['id_user'])
+conversion_por_gclid= Conversiones(navegacion_final['gclid1'], conversion_final['gclid'])
+# creamos la union de los datos
+union = {'Campaña':navegacion_final['Campaña'], 'Adgroup':navegacion_final['Adgroup'], 'Advertisement':navegacion_final['Advertisement'], 'Site_link':navegacion_final['Site_link'], 'id_user_navegacion':navegacion_final['id_user1'], 'gclid_navegacion':navegacion_final['gclid1']}
+union_final= pd.DataFrame(union)
+csv_union= union_final.assign(conversion_id=conversion_por_id, conversion_gclid=conversion_por_gclid)
 
 
 
